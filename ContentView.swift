19841dfce8cdf28795a121ac25238a6bf56a02ff
Khadija <kazam8513@stu.d214.org>
2025-04-07 13:33:08 -0500

    added a robot

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var username = ""
     var body: some View {
         VStack {
             Text("Khadija Azam")
+            Text("Alex V")
+            
+            Text("Hello, I'm a robot")
+                .font(.largeTitle)
+                .bold()
+            Circle()
+                .frame(width: 100,height: 29)
+            
+            
+            // STICKMAN
+            HStack{
+                Ellipse()
+                    .frame(width: 15, height: 80)
+                    .rotationEffect(.degrees(-160))
+                    .offset(x: 1)
+                Ellipse()
+                    .frame(width: 30, height: 90)
+                    .offset(x: 2)
+                Ellipse()
+                    .frame(width: 13, height: 70)
+                    .rotationEffect(.degrees(240))
+                    .offset(x: 20, y: -50)
+            }
+            HStack{
+                Ellipse()
+                    .frame(width: 15, height: 50)
+                    .rotationEffect(.degrees(20))
+                    .offset(x: -12, y: -9)
+                Ellipse()
+                    .frame(width: 15, height: 50)
+                    .rotationEffect(.degrees(-20))
+                    .offset(x: 12, y: -9)
+                
+               
+            }
+            TextField("What's your name?", text: $username)
+                .textFieldStyle(.roundedBorder)
+            
+            Text("Hello, \(username)")
+            
         }
-       Text("Alex V")
     }
 }
