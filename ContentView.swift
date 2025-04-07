f753a5bb4e03195181f921bcaecf4bce57c5f2e3
slackappu <alexveliyathumalil21@gmail.com>
2025-04-07 11:23:57 -0700

    added a system image with opacity and color

--- ContentView.swift
+++ ContentView.swift
@@ -4,7 +4,10 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Khadija Azam")
+            Text("Alex V")
+            Image(systemName: "apple.terminal.on.rectangle")
+                .foregroundStyle(.purple)
+                .opacity(0.7)
         }
-       Text("Alex V")
     }
 }
